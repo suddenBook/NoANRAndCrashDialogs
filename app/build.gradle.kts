02506fdcf4602d94f3b65abe9789bb37c37aa58a
Desmond <huixiaqingliu@outlook.com>
2025-11-02 19:05:03 +0800

    Add support for Android 13 & Add auto compile using GitHub Actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,22 +5,35 @@ plugins {
 
 android {
     namespace = "com.diskree.noanrandcrashdialogs"
-    compileSdk = 35
+    compileSdk = 33
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("APP_RELEASE_STORE_FILE") ?: project.property("APP_RELEASE_STORE_FILE") as String)
+            storePassword = System.getenv("APP_RELEASE_STORE_PASSWORD") ?: project.property("APP_RELEASE_STORE_PASSWORD") as String
+            keyAlias = System.getenv("APP_RELEASE_KEY_ALIAS") ?: project.property("APP_RELEASE_KEY_ALIAS") as String
+            keyPassword = System.getenv("APP_RELEASE_KEY_PASSWORD") ?: project.property("APP_RELEASE_KEY_PASSWORD") as String
+        }
+    }
 
     defaultConfig {
-        minSdk = 35
-        targetSdk = 35
+        minSdk = 33
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles("proguard-rules.pro")
-            signingConfig = signingConfigs["debug"]
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
         }
+        debug {}
     }
 
     kotlin {
